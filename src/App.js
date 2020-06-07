9c7f2e1d6fe30fd95e3a3d1a04ddb589ea0880e9
Derek Zhan <derekjan1240@gmail.com>
2020-06-08 00:09:16 +0800

    handle routes

--- src/App.js
+++ src/App.js
@@ -3,41 +3,33 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { reducer } from './reducer'
-import { 
-  Grid,
-  Button,
-} from '@material-ui/core';
 import Home from './components/Home';
-import About from './components/About';
+import AuthSwitch from './components/AuthSwitch';
+import Characters from './components/Characters'
+import Navbar from './components/Navbar';
+import './fontawesome';
+
+// Swalalert
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+const MySwal = withReactContent(Swal)
+
 
 const store = createStore(reducer);
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Grid 
-          container
-          alignItems="center"
-          justify="center"
-          spacing={2}>
-          <Grid item xs={12} style={{textAlign:"center"}}>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQn2wyQqnGuNSOGOJJavVTLF6KNF4yM40ahl4N4kzRmx0J_iTAC&usqp=CAU' alt="" style={{width:"5%", borderRadius:"50%", marginTop:30}}/>
-          </Grid>
-          <Grid item>
-            <Button href="/" color="primary">Home</Button>
-          </Grid>
-          <Grid item>
-            <Button href="/About" color="primary">About</Button>
-          </Grid>
-        </Grid>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-          <Route component={Error} />
-        </Switch>
-      </BrowserRouter>
       <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/AuthSwitch" component={AuthSwitch} />
+            <Route path="/Characters" component={Characters} />
+            <Route component={Error} />
+          </Switch>
+        </BrowserRouter>
       </Provider>
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+import { 
+    makeStyles,
+    Grid,
+    Button,
+} from '@material-ui/core';
+
+const SITE_ICON = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQn2wyQqnGuNSOGOJJavVTLF6KNF4yM40ahl4N4kzRmx0J_iTAC&usqp=CAU';
+
+const useStyles = makeStyles({
+    imageWarp: {
+        textAlign: "center",
+    },
+    image: {
+        width: "5%",
+        borderRadius: "50%",
+        marginTop: 30,
+    },
+});
+
+function Navbar() {
+    const history = useHistory();
+    const classes = useStyles();
+
+    function handleClick(page) {
+        history.push(page);
+    }
+
+    return (
+        <>
+        <Grid
+            container
+            alignItems="center"
+            justify="center"
+            spacing={2}>
+            <Grid item xs={12} className={classes.imageWarp}>
+                <img src={SITE_ICON} alt="QQ" className={classes.image}/>
+            </Grid>
+            <Grid item>
+                <Button color="primary" onClick={ () => handleClick('/')}>Home</Button>
+            </Grid>
+            <Grid item>
+                <Button color="primary" onClick={ () => handleClick('/AuthSwitch')}>AuthSwitch</Button>
+            </Grid>
+            <Grid item>
+                <Button color="primary" onClick={ () => handleClick('/Characters')}>Characters</Button>
+            </Grid>
+        </Grid>
+        </>
+    )
+}
+
+export default Navbar;
