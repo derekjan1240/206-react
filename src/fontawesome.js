53e41b5b5a7fae02225dd784e06d8cb4871f22dc
Derek Zhan <derekjan1240@gmail.com>
2020-06-08 00:07:50 +0800

    add font-awesome swal2

---
+++ src/fontawesome.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import { faChevronCircleRight, faChevronCircleLeft, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+    faChevronCircleRight,
+    faChevronCircleLeft,
+  // more icons go here
+);
